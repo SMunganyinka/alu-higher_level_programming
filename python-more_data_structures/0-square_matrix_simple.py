b07dd6a5e4b417d210d7f4ff807533fe3f235e60
SMunganyinka <s.munganyin@alustudent.com>
2025-02-25 05:46:06 -0800

    a function that computes the square value of all integers of a matrix.

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
+
 def square_matrix_simple(matrix=[]):
     """
     Computes the square value of all integers in a 2D matrix.
-    
+
     Args:
         matrix (list of lists): A 2D list containing integers.
 
