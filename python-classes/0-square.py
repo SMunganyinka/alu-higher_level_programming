06e20ae7379eceb5a750149b7e2b5ada2d4e816b
SMunganyinka <s.munganyin@alustudent.com>
2025-03-06 04:03:51 -0800

    an empty class Square that defines a square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,6 @@
-#!/usr/bin/python3
 class Square:
     """An empty class that defines a square."""
-    pass
+    
+    def __init__(self):
+        """Initialize an instance of Square."""
+        pass
