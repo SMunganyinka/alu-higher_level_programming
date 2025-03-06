510ddcb05266865a55490352d82c1dda4e192334
SMunganyinka <s.munganyin@alustudent.com>
2025-03-06 04:56:18 -0800

    a class Square that defines a square by: (based on 1-square.py)

---
+++ python-classes/2-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+class Square:
+    """
+    A class that defines a square by its size.
+
+    Attributes:
+        __size (int): The size of the square (private attribute).
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new square with a given size.
+
+        Args:
+            size (int): The size of the square. Default is 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size  # Private instance attribute
