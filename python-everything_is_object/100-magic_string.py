d0b7ee966a749052a3af6e2f760ce87ebc7e450f
SMunganyinka <s.munganyin@alustudent.com>
2025-03-07 05:46:24 -0800

    a function magic_string() that returns a string “BestSchool” n times the number of the iteration

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def magic_string(n):
-    return "".join(["BestSchool" * i for i in range(1, n+1)])
+    return ''.join(['BestSchool' * i for i in range(1, n+1)])
+print(magic_string(1))
