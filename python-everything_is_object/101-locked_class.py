71d730c0ced06966805147efaef2a44579fe0677
SMunganyinka <s.munganyin@alustudent.com>
2025-03-07 06:02:36 -0800

    a class LockedClass with no class or object attribute, that prevents the user from dynamically creating new instance attributes, except if the new instance attribute is called first_name.

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,3 +1,18 @@
 #!/usr/bin/python3
+"""
+This module defines a LockedClass that restricts dynamic attribute creation.
+
+Users can only create an instance attribute called 'first_name'.
+Any attempt to create other attributes will raise an AttributeError.
+"""
+
+
 class LockedClass:
+    """
+    A class that prevents dynamic attributes except 'first_name'.
+
+    Attributes:
+        first_name (str): Allowed instance attribute.
+    """
+
     __slots__ = ['first_name']
