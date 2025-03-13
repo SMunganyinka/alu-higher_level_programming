da91b61be7f9dd2920ea99d030edff14af9e2982
SMunganyinka <s.munganyin@alustudent.com>
2025-03-13 08:35:04 -0700

    qn17

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,14 +1,12 @@
--- Use the specified database
+-- Select the database
 USE hbtn_0c_0;
 
--- Set the database character set to utf8mb4 (if not already set)
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Drop the table if it exists (be careful, you will lose existing data)
+DROP TABLE IF EXISTS first_table;
 
--- Convert the table character set to utf8mb4
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Modify the 'name' column to remove CHARACTER SET but keep COLLATE
-ALTER TABLE first_table MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
-
--- Final check - you can run this to verify later
-SHOW CREATE TABLE first_table;
+-- Recreate the table with the correct collation and charset
+CREATE TABLE first_table (
+    id INT DEFAULT NULL,
+    name VARCHAR(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    score INT DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
