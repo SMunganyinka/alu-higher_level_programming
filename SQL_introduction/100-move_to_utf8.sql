5a2b7b97e494f1745d9b7bc4f8b2cefa8f98c62f
SMunganyinka <s.munganyin@alustudent.com>
2025-03-13 07:53:12 -0700

    qn17

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,14 +1,11 @@
--- Convert the database hbtn_0c_0 to UTF8 (utf8mb4, utf8mb4_unicode_ci)
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Select the database to use.
+-- Select the database
 USE hbtn_0c_0;
 
--- Convert the table first_table to UTF8
+-- Convert the database to utf8mb4 (charset and collation)
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Convert the table to utf8mb4 (charset and collation)
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Convert the field name in first_table to UTF8
+-- Modify the 'name' column only to change the collation
 ALTER TABLE first_table MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
-
--- Ensure score column is present
-ALTER TABLE first_table MODIFY score INT DEFAULT NULL;
