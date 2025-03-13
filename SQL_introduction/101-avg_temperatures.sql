a0122f8e34693fff1df40c151766eec388bc5d74
SMunganyinka <s.munganyin@alustudent.com>
2025-03-13 08:53:53 -0700

    qn18

---
+++ SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,8 @@
+-- Select the correct database
+USE hbtn_0c_0;
+
+-- Query to display average temperature by city ordered by temperature (descending)
+SELECT city, AVG(temperature) AS avg_temp
+FROM weather_data  -- Assuming the table name is 'weather_data'
+GROUP BY city
+ORDER BY avg_temp DESC;
