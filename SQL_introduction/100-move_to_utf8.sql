aa6d74bd0b66e5609bcfba186920ea630b242f02
SMunganyinka <s.munganyin@alustudent.com>
2025-03-13 07:55:13 -0700

    qn17

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -7,5 +7,5 @@ ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- Convert the table to utf8mb4 (charset and collation)
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Modify the 'name' column only to change the collation
+-- Modify the 'name' column to change the collation but NOT the charset
 ALTER TABLE first_table MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
