88e5e4b8d48b8def240f013c92a067b9c25d8b78
SMunganyinka <s.munganyin@alustudent.com>
2025-03-13 08:29:11 -0700

    qn17

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,14 @@
--- Select the correct database
+-- Use the specified database
 USE hbtn_0c_0;
 
--- Alter the 'name' column to remove CHARACTER SET and keep COLLATE only
+-- Set the database character set to utf8mb4 (if not already set)
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Convert the table character set to utf8mb4
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Modify the 'name' column to remove CHARACTER SET but keep COLLATE
 ALTER TABLE first_table MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
+
+-- Final check - you can run this to verify later
+SHOW CREATE TABLE first_table;
