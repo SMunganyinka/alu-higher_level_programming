7f1152b2d08748d5447dc50e49b4515201da5022
SMunganyinka <s.munganyin@alustudent.com>
2025-03-13 08:49:47 -0700

    qn17

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,11 +1,14 @@
 -- Select the correct database
 USE hbtn_0c_0;
 
--- Drop the 'name' column (this will remove the column but not the data)
+-- Drop the 'name' column
 ALTER TABLE first_table DROP COLUMN name;
 
--- Recreate the 'name' column with the correct collation and **no charset** definition
+-- Recreate the 'name' column with the correct collation and NO charset definition
 ALTER TABLE first_table ADD COLUMN name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
+
+-- Convert the table to the correct character set and collation.
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- Check if the changes have been applied correctly
 SHOW CREATE TABLE first_table;
