9ce0c52d5fc2eb435af8a1fee04701cc59b1b012
SMunganyinka <s.munganyin@alustudent.com>
2025-03-16 10:31:59 -0700

    a Python script that prints exactly and that piece of art is useful - Dora Korpar, 2015-10-19, followed by a new line.

---
+++ python-hello_world/100-write.py
@@ -0,0 +1,7 @@
+import sys
+
+# Write the required text to stderr using sys.stderr.write
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+
+# Exit with status code 1
+sys.exit(1)
