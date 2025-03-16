c10c0f068833e43f4342b1c065d2da695f8ff05b
SMunganyinka <s.munganyin@alustudent.com>
2025-03-16 12:56:34 -0700

    a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -6,5 +6,13 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-# Use curl to send the request and display the size of the body of the response
-curl -sI "$1" | grep -i 'Content-Length' | awk '{print $2}'
+# Get the size of the response body using curl and Content-Length
+size=$(curl -sI "$1" | grep -i 'Content-Length' | awk '{print $2}')
+
+# Check if the size is available and display #
+if [ -n "$size" ]; then
+  printf '#%.0s' $(seq 1 "$size")
+  echo
+else
+  echo "Error: Could not retrieve Content-Length"
+fi
