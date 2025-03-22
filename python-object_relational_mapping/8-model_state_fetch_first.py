5a9bfd31d4298e8a19b2df5f4413a9bc645de054
SMunganyinka <s.munganyin@alustudent.com>
2025-03-22 07:15:07 -0700

    a script that prints the first State object from the database hbtn_0e_6_usa

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+a script that prints the first State object from the database
+"""
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True
+    )
+
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    row = session.query(State).first()
+
+    if row:
+        print("{}: {}".format(row.id, row.name))
+    else:
+        print("Nothing")
+    session.close()
