9eefc6b8a0ae89d90b989ebac22682504b7bd358
SMunganyinka <s.munganyin@alustudent.com>
2025-05-27 03:41:19 -0700

    qn17

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
+
 const myObject = {
   type: 'object',
   value: 12
 };
+
+// Log the object before adding the `incr` function
 console.log(myObject);
 
 myObject.incr = function () {
@@ -11,74 +14,9 @@ myObject.incr = function () {
 
 myObject.incr();
 console.log(myObject);
+
 myObject.incr();
 console.log(myObject);
+
 myObject.incr();
 console.log(myObject);
-
-
-
-
-- Summary report for where each system falls out for example airtable is in this section and it does the following
-- Have the system recommendation in a table format with pro's and con's 
-  - Main features 
-  - Price 
-  - Pro's and Con's
-  - Number of users
-
-- The grant management application is for tracking applications and should help in outlining the methods for discovering and selecting projects or initiatives that
-align with the Foundation’s mission.
-
-- Wording needs to be more operational and support 
-- Contracting for employees signing recruiment (Docusign )
-- The systems security standards should be standard and ensures that even when they change it should be this is the standard for whatever standard they choose.
-
-- Set Systems and administration as a table on about System and implmenetation timeline. (Then thy'll select and we choose)
-
-  - In the table about systems have the pricing and cost analysis as a column.
-
-  - Include descriptions to Hardware recommendations to make sure they fit into the standards and the overal framework
-
-- Have a session where we discuss how each systems and technology solution will contribute to the overall goal of the operational framework we are building
-
-
-[Service level agreement -they need to have SLA's for all the providers]
-
-
-
-Attached are two docs -- One is the Inception report highlighting our work plan and what we will be providing
-Secondaly is the doc I created highlighting like a framework outline not a framework outline though, it more of what needs thinking about and 
-the help I need is in structuring a full on document based of the inception report that would be submitted as the deliverable of the work.
-
-So your task in light of the above as well is to simply do this help develop a robust operational framework. 
-We will adapt to existing WRI Policies, identify gaps and create policies/procedures for VIA Foundation's needs. We will asses and recommend essential systems (e.g. grants management systems) and support their implementation, ensuring alignment with ops framework
-
-
-So basically, let's focus on the technological factor of the project. our focus for this documnent needs to be on Systems & Tools: the document framework should
-indicate the key digital platforms or processes supporting the Foundation’s operations (e.g., finance software, communication tools). 
-
-it should recommend specific tools based of the above data i shared, and give solutions to 
-Data Management by Explaining how data is collected, stored, and protected to maintain confidentiality
-and integrity.
-
-It should also have high level IT Policies section this will summarizes the Foundation’s approach to preventing data breaches, managing user
-access, and handling technological risks.
-
-It should have a comprehensive Systems assessment and recommendation including:
-- Requirement definition
-- Identify the automation needs specific to VIA’s grant management processes—application intake,
-evaluation, disbursement, monitoring, and reporting.
-- Determine additional operational requirements.
-- Solution evaluation, Evaluate multiple software platforms, focusing on scalability, compliance, cost, usability, integration
-capabilities, and data security.
-- Have a Recommendation report
-- Provide a comparative analysis and recommend the most suitable system(s).
-- Outline an approximate implementation timeline and considerations for integration.
-- Liaison with vendor (Not Installation):
-- Offer guidance to ensure the vendor’s configuration aligns with the new operational framework.
-
-Note: The consultancy which is me does not include direct system installation or configuration, as those activities remain with the vendor.
-
-So with every solution, framework and the tool you are going to recommend you need to highlight the why, why they are going to use that particular tool
-how many people can use the tool, the learning gap, the cost breakdown, the inter-connection and platform dependency (meaning how does it intergrate with other tools) 
-the security features and other just think about other question they'd ask in a meeting about the recommendation.
